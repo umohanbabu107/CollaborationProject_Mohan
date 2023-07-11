542ad88e275d613c35802732f107ae57119b4447
Mohan <umohantest1@gmail.com>
2023-07-11 18:28:20 +0530

    Test case 3 added

--- src/test/java/gitPractice/git/Test1.java
+++ src/test/java/gitPractice/git/Test1.java
@@ -3,17 +3,22 @@ package gitPractice.git;
 import org.testng.annotations.Test;
 
 public class Test1 {
-	
+
 	@Test
 	public void testCase1() {
 		// Mohan1
 		System.out.println("Test case 1");
 	}
-	
+
 	@Test
 	public void testCase2() {
 		// Mohan2
 		System.out.println("Test case 2");
+	}
+
+	@Test
+	public void testCase3() {
+		System.out.println("Test case 3");
 	}
 
 }
