3ff967e06830b66d9049e6af858e2f1309583a9a
Mohan1 <mohantest1@gmail.com>
2023-07-11 17:46:40 +0530

    Mohan1 commit1

---
+++ src/test/java/gitPractice/git/Test1.java
@@ -0,0 +1,13 @@
+package gitPractice.git;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void testCase1() {
+		// Mohan1
+		System.out.println("Test case 1");
+	}
+
+}
